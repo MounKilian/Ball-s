52d837ae6b34b5435f85b99b51cc7961de8060ff
mkilian <kilian.moun@ynov.com>
2024-06-03 21:36:07 +0200

    fix merge

--- dbp/manage_db.go
+++ dbp/manage_db.go
@@ -23,14 +23,14 @@ func RegisterUser(username, email, password string) error {
 	var existingUser User
 	err := DB.Where("username = ? OR email = ?", username, email).First(&existingUser).Error
 	if err == nil {
-		return errors.New("Un utilisateur avec le même nom d'utilisateur ou la même adresse e-mail existe déjà")
+		return errors.New("un utilisateur avec le même nom d'utilisateur ou la même adresse e-mail existe déjà")
 	} else if !errors.Is(err, gorm.ErrRecordNotFound) {
-		return errors.New("Erreur lors de la recherche de l'utilisateur dans la base de données")
+		return errors.New("erreur lors de la recherche de l'utilisateur dans la base de données")
 	}
 
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
-		return errors.New("Erreur lors du hachage du mot de passe")
+		return errors.New("erreur lors du hachage du mot de passe")
 	}
 
 	user := User{
@@ -40,7 +40,7 @@ func RegisterUser(username, email, password string) error {
 	}
 	err = DB.Create(&user).Error
 	if err != nil {
-		return errors.New("Erreur lors de l'enregistrement de l'utilisateur dans la base de données")
+		return errors.New("erreur lors de l'enregistrement de l'utilisateur dans la base de données")
 	}
 
 	return nil
@@ -50,12 +50,12 @@ func AuthenticateUser(username, password string) (bool, error) {
 	var user User
 	err := DB.Where("username = ?", username).First(&user).Error
 	if err != nil {
-		return false, errors.New("Nom d'utilisateur incorrect")
+		return false, errors.New("nom d'utilisateur incorrect")
 	}
 
 	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
 	if err != nil {
-		return false, errors.New("Mot de passe incorrect")
+		return false, errors.New("mot de passe incorrect")
 	}
 
 	return true, nil
