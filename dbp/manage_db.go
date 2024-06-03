f22bbf9d60603cf6dc5ea0af493966fc8dd84cf4
delhadjm <cirelaria@gmail.com>
2024-06-03 16:47:16 +0200

    l'auth de deux users avec les meme infos est desormais interdit

--- dbp/manage_db.go
+++ dbp/manage_db.go
@@ -1,6 +1,7 @@
 package dbp
 
 import (
+	"errors"
 	"log"
 
 	"golang.org/x/crypto/bcrypt"
@@ -19,9 +20,17 @@ func init() {
 }
 
 func RegisterUser(username, email, password string) error {
+	var existingUser User
+	err := DB.Where("username = ? OR email = ?", username, email).First(&existingUser).Error
+	if err == nil {
+		return errors.New("Un utilisateur avec le même nom d'utilisateur ou la même adresse e-mail existe déjà")
+	} else if !errors.Is(err, gorm.ErrRecordNotFound) {
+		return errors.New("Erreur lors de la recherche de l'utilisateur dans la base de données")
+	}
+
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
-		return err
+		return errors.New("Erreur lors du hachage du mot de passe")
 	}
 
 	user := User{
@@ -31,24 +40,23 @@ func RegisterUser(username, email, password string) error {
 	}
 	err = DB.Create(&user).Error
 	if err != nil {
-		return err
+		return errors.New("Erreur lors de l'enregistrement de l'utilisateur dans la base de données")
 	}
 
 	return nil
 }
 
-
 func AuthenticateUser(username, password string) (bool, error) {
 	var user User
 	err := DB.Where("username = ?", username).First(&user).Error
 	if err != nil {
-		return false, err
+		return false, errors.New("Nom d'utilisateur incorrect")
 	}
 
 	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
 	if err != nil {
-		return false, nil // Mot de passe incorrect
+		return false, errors.New("Mot de passe incorrect")
 	}
 
-	return true, nil // Authentification réussie
+	return true, nil
 }
