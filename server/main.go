1ea4df83caa314f1ca12142dd52191c3de3250a4
peanthony <anthony.pereira@ynov.com>
2024-06-05 14:45:35 +0200

    progressed profileUser

--- server/main.go
+++ server/main.go
@@ -172,7 +172,7 @@ func profileUser(c *gin.Context) {
 		Username:  "username",
 		Email:     "email@email.com",
 		Password:  "Password",
-		Image:     "backarrow.svg",
+		Image:     "uglyprofilpic.png",
 		Biography: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
 		SportID:   1,
 		Sport: dbp.Stat{
