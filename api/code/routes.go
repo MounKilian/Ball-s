6625be2435afa7b0226820d5d26988e08cdfec15
Tokennn <quentin.contreau@outlook.fr>
2024-06-11 09:21:15 +0200

    feat : lot of stuff

--- api/code/routes.go
+++ api/code/routes.go
@@ -5,17 +5,17 @@ package api
 import "net/http"
 
 func SetupRoutes() {
-	http.HandleFunc("/register", serveRegisterForm)
+	//http.HandleFunc("/register", serveRegisterForm)
 	http.HandleFunc("/users/create", createUser)
-	// http.HandleFunc("/login", login)
-	http.HandleFunc("/profile", profile)
+	//http.HandleFunc("/login", login)
+	//http.HandleFunc("/profile", profile)
 	http.HandleFunc("/profile/update", updateProfile)
 	http.HandleFunc("/profile/delete", deleteUser)
-	http.HandleFunc("/home_connected", homeConnected)
+	//http.HandleFunc("/home_connected", homeConnected)
 	http.HandleFunc("/post/create", createPost)
 	http.HandleFunc("/post/update", updatePost)
 	http.HandleFunc("/post/delete", deletePost)
-	http.HandleFunc("/posts", getPosts)
+	//http.HandleFunc("/posts", getPosts)
 
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 }
